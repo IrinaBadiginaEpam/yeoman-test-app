8f41d3588865d48849ae2bb17bb2cf14266d5d99
Badigina, Irina <irina.badigina@kronos.com>
2018-10-30 15:23:12 +0300

    Update tests to use enzyme

--- src/tests/CounterContainer.test.jsx
+++ src/tests/CounterContainer.test.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+
 import { CounterContainer } from '../containers/CounterContainer';
 
+const props = {
+  increment: () => {},
+  decrement: () => {},
+  incrementIfOdd: () => {},
+  counter: 0
+};
+
 it('renders without crashing', () => {
-  const div = document.createElement('div'),
-  props = {
-    increment: () => {},
-    decrement: () => {},
-    incrementIfOdd: () => {},
-    counter: 0
-  };
+  const div = document.createElement('div');
   ReactDOM.render(<CounterContainer {...props}/>, div);
+});
+
+it('matches snapshot', () => {
+  const container = shallow(<CounterContainer {...props}/>);
+
+  expect(container).toMatchSnapshot();
 });
