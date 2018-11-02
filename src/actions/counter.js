9b79ff411927e6751f7009b7e19e65bc0a5c15fc
Badigina, Irina <irina.badigina@kronos.com>
2018-11-02 15:36:51 +0300

    Fix code style issues and relative paths

--- src/actions/counter.js
+++ src/actions/counter.js
@@ -1,18 +1,18 @@
-import { INCREMENT_COUNTER, DECREMENT_COUNTER } from 'constants/ActionTypes'
-import { createAction } from 'redux-actions'
+import { INCREMENT_COUNTER, DECREMENT_COUNTER } from '../constants/ActionTypes';
+import { createAction } from 'redux-actions';
 
-export const increment = createAction(INCREMENT_COUNTER)
+export const increment = createAction(INCREMENT_COUNTER);
 
-export const decrement = createAction(DECREMENT_COUNTER)
+export const decrement = createAction(DECREMENT_COUNTER);
 
 export function incrementIfOdd() {
   return (dispatch, getState) => {
-    const { counter } = getState()
+    const { counter } = getState();
 
     if (counter % 2 === 0) {
-      return
+      return;
     }
 
-    dispatch(increment())
+    dispatch(increment());
   }
 }
--- src/store/store.dev.js
+++ src/store/store.dev.js
@@ -1,21 +1,21 @@
-import { createStore, applyMiddleware } from 'redux'
-import ReduxThunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import rootReducer from 'reducers'
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from '../reducers';
 
 export default function configureStore(initialState = {}) {
-  const middlewares = [ReduxThunk]
+  const middlewares = [ReduxThunk];
   const enhancers = [
     applyMiddleware(...middlewares),
     // other store enhancers if any
-  ]
+  ];
   const composeEnhancers = composeWithDevTools(
     {
       // other compose enhancers if any
       // Specify here other options if needed
     }
-  )
-  const store = createStore(rootReducer, initialState, composeEnhancers(...enhancers))
+  );
+  const store = createStore(rootReducer, initialState, composeEnhancers(...enhancers));
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
     module.hot.accept('../reducers', () => {
@@ -25,5 +25,5 @@ export default function configureStore(initialState = {}) {
     })
   }
 
-  return store
+  return store;
 }
